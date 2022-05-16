e78436196a95cfb730739f55162c083809218914
Adam Thomas <adam@Adams-MacBook-Pro.local>
2022-05-15 20:41:32 -0400

    Added square, rect, circle and ellipse

---
+++ shapes/src/main/kotlin/Ellipse.kt
@@ -0,0 +1,10 @@
+class Elipse(var center: Point, var xRadius: Double, var yRadius: Double): Shape() {
+    override fun getArea(): Double {
+        TODO("Not yet implemented")
+    }
+
+    override fun move(xDiff: Double, yDiff: Double) {
+        TODO("Not yet implemented")
+    }
+
+}
