872a17a9057b51c50eefc78e5654bf7c3db494eb
Adam Thomas <adam@Adams-MacBook-Pro.local>
2022-05-15 23:23:26 -0400

    Cleaned up code and added comments

--- shapes/src/main/kotlin/Shape.kt
+++ shapes/src/main/kotlin/Shape.kt
@@ -3,8 +3,9 @@ import kotlin.math.sqrt
 
 abstract class Shape {
 
+    // Calculates the area of a shape based on what type it is
     abstract fun getArea(): Double
 
-
+    // Moves all points for a shape by an x differential and a y differential
     abstract fun move(xDiff: Double, yDiff: Double)
 }
